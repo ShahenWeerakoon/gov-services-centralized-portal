43072854d6ff731c2eb2cd6fdc981ea9717334a4
Shahen Weerakoon <shahenweerakoon2023@gmail.com>
2025-08-20 00:36:25 +0530

    Paths+ navbar

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "./About.css";
+import "../styles/About.css";
 
 const About = () => {
   const [aboutData, setAboutData] = useState(null);
